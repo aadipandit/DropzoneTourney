6f8bbefd075b7189b6bdef87181abca5d6c0afbb
aadipandit <134945313+aadipandit@users.noreply.github.com>
2024-09-05 18:35:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript code to enhance the website experience
+// Add more dynamic effects or interactivity if needed
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to DropZoneTourney!");
+});
